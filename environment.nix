7c04d168ca9a5d8d1ed36dc0bea2bc2f4bed1a72
Marco Stolle <marco.stolle@gmail.com>
2026-04-26 18:07:56 +0200

    added console.keyMap to environment

--- environment.nix
+++ environment.nix
@@ -1,5 +1,6 @@
 { pkgs }:
 {
+ console.keyMap = "be-latin1";
  environment.systemPackages = with pkgs; [
    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
    wget
